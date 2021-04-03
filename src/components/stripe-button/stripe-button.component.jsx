d5f8d73c8db708465667433d09dd8bb9cbd714f5
Bharat Kammakatla <bharatkammakatla@gmail.com>
2021-04-03 12:09:01 +0800

    updating the stripe integration and updating our favicon and title of the application

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IagJNSEL9imDsIXnLrrsQBD2736GRJi6Jy34TVss0FfQ59fehnjFD41ABcW56S3M8k1XV5TJ8uJSBdwhjzd5peL00BZoz3uwg";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Your Shop Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
