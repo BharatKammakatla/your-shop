12867cb3b498165ff4326a1897dde38020715d04
Bharat Kammakatla <bharatkammakatla@gmail.com>
2021-04-05 23:45:56 +0800

    Replaced scss of all page components with styled css

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Directory from "../../components/directory/directory.components";
-import { HomePageContainer } from "./homepage.styles";
+import { HomePageContainer } from "./homepage.styled";
 
 const HomePage = () => (
   <HomePageContainer>
